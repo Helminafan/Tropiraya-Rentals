7e6c85827e6574de743ef00b5b04b09be58a1c62
Helminafan <nafan2332@gmail.com>
2023-11-24 09:01:37 +0800

    add desain dahboard and barang

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+Route::get('/admin/barang', function () {
+    return view('admin.barang');
+})->name('admin.barang');
 
 Route::middleware([
     'auth:sanctum',
