ee782e5a2d77f6910ef6976223b40945de22237b
Helminafan <nafan2332@gmail.com>
2023-11-28 11:47:37 +0800

    edit login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/auth/login');
+    return view('/auth/logi');
 });
 
 Route::get('/admin/barang', function () {
