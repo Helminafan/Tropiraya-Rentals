c000331ee6cf78587823437b7bf667884f48c63f
Helminafan <nafan2332@gmail.com>
2023-11-28 12:12:15 +0800

    admin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/auth/login');
+    return view('welcome');
 });
 
 Route::get('/admin/barang', function () {
