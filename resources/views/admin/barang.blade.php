512d31b5afd972eeca044b3ba5d8f7fc4ecf6858
nadiarizatulriski <nadia.210170209@mhs.unimal.ac.id>
2023-11-28 12:50:39 +0700

    add desain dashboard dan barang

---
+++ resources/views/admin/barang.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.master')
+@section('admin')
+<div class="container-fluid" style="margin-top: 50px">
+            <!-- Page Heading -->
+            <h1 class="h3 mb-2 text-gray-800">Barang-Barang</h1>
+            <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables,</p>
+
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+              <div class="card-header py-3">
+                <div class="row">
+                  <h6 class="col m-0 font-weight-bold text-primary">DataTables Example</h6>
+                  <div class="co">
+                    <a href="tambahbarang.html" class="btn btn-warning btn-icon-split"
+                      ><span class="icon text-white-50">
+                        <i class="fas fa-plus"></i>
+                      </span>
+                      <span class="text">Tambah Barang</span></a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                      <tr class="text-center">
+                        <th>Foto</th>
+                        <th>Nama Barang</th>
+                        <th>Merek</th>
+                        <th>Jumlah</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr class="text-center">
+                        <td><img src="img/p1.png.png" width="70px" alt="" /></td>
+                        <td>Bola Sepak</td>
+                        <td>Adidas</td>
+                        <td>30</td>
+                        <td>
+                          <div class="row">
+                            <a href="editbarang.html" class="col btn btn-success" style="margin: 0 10px">Edit</a>
+                            <a href="" class="col btn btn-danger" style="margin: 0 10px">Hapus</a>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr class="text-center">
+                        <td><img src="img/pngwing.com (1).png" width="70px" alt="" /></td>
+                        <td>Bola Basket</td>
+                        <td>Adidas</td>
+                        <td>30</td>
+                        <td>
+                          <div class="row">
+                            <a href="" class="col btn btn-success" style="margin: 0 10px">Edit</a>
+                            <a href="" class="col btn btn-danger" style="margin: 0 10px">Hapus</a>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr class="text-center">
+                        <td><img src="img/pngwing.com (2).png" width="70px" alt="" /></td>
+                        <td>Bola Voli</td>
+                        <td>Adidas</td>
+                        <td>30</td>
+                        <td>
+                          <div class="row">
+                            <a href="" class="col btn btn-success" style="margin: 0 10px">Edit</a>
+                            <a href="" class="col btn btn-danger" style="margin: 0 10px">Hapus</a>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+@endsection
---
+++ resources/views/admin/component/sidebar.blade.php
@@ -0,0 +1,58 @@
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar">
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+          <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+          </div>
+          <img style="width: 150 px" height="35" src="{{asset('admin/img/Frame 22.png')}}" alt="" />
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0" />
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item  {{ Request()->is('admin/dashboard')?'active': ''}}">
+          <a class="nav-link" href="{{route('admin.dashboard')}}">
+            <i class="fa-brands fa-microsoft"></i>
+            <span>Dashboard</span></a
+          >
+        </li>
+
+        <!-- Divider -->
+
+        <!-- Nav Item - Charts -->
+        <li class="nav-item  {{ Request()->is('admin/barang')?'active': ''}}">
+          <a class="nav-link" href="{{route('admin.barang')}}">
+            <i class="fa-solid fa-box"></i>
+
+            <span>Barang</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="peminjam.html">
+            <i class="fa-solid fa-layer-group"></i>
+            <span>Peminjam</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="riwayatpeminjaman.html">
+            <i class="fa-solid fa-clock-rotate-left"></i>
+            <span>Riwayat Peminjam</span></a
+          >
+        </li>
+
+        <!-- Nav Item - Tables -->
+        <li class="nav-item position-absolute" style="bottom: 0">
+          <a class="nav-link" href="tables.html">
+            <i class="fa-solid fa-right-from-bracket"></i>
+            <span>Log Out</span></a
+          >
+        </li>
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+          <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+
+        <!-- Sidebar Message -->
+      </ul>
