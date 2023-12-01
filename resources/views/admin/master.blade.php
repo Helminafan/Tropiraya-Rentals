65a32bd08fef4a9779256f17360f9b50040b120f
Helminafan <nafan2332@gmail.com>
2023-12-02 05:41:27 +0800

    fixed

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('admin/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
   </head>
 
   <body id="page-top">
