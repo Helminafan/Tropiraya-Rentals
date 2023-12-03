0619880e1158ce1e651928b1b382032195209094
dyalamsyah <riantedialasmyah@gmail.com>
2023-12-03 13:00:09 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
