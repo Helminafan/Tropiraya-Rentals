ee8ec699f9112795d1731be139fa18257ac37716
Helminafan <nafan2332@gmail.com>
2023-12-06 11:53:15 +0800

    add detail barang

--- resources/views/user/detailbarang.blade.php
+++ resources/views/user/detailbarang.blade.php
@@ -10,18 +10,18 @@
           <div class="row">
             <div class="col box">
               <div class="img-box" style="height: 300px">
-                <img src="{{asset ('user/images/p1.png.png') }}" alt="" />
+                <img src="{{asset ('storage/'.$data->fotobarang) }}" alt="" />
               </div>
             </div>
             <div class="col">
-              <h1 style="font-weight: 700; font-family: Arial, Helvetica, sans-serif; margin-top: 10px;">Bola Sepak</h1>
+              <h1 style="font-weight: 700; font-family: Arial, Helvetica, sans-serif; margin-top: 10px;">{{$data->nam}}</h1>
               <h3 style=" font-weight: 300; font-family: Arial, Helvetica, sans-serif">Spesifikasi Produk</h3>
               <div class="row" style="margin-top: 20px">
                 <div class="col">
                   <h6>Merek :</h6>
                 </div>
                 <div class="col" style="margin-right: 300px">
-                  <h5>Adidas</h5>
+                  <h5>{{$data->merek}}</h5>
                 </div>
               </div>
               <div class="row" style="margin-top: 10px">
@@ -29,12 +29,12 @@
                   <h6>Jumlah :</h6>
                 </div>
                 <div class="col" style="margin-right: 300px">
-                  <h5>56</h5>
+                  <h5>{{$data->jumlahBarang}}</h5>
                 </div>
               </div>
 
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores incidunt perspiciatis eveniet odit earum a pariatur perferendis dolor, itaque culpa?</p>
-                <a href="{{route ('user.peminjaman') }}"> Pinjam Sekarang </a>
+              <p>{{$data->deskripsiBarang}}</p>
+                <a href="{{route ('user.formSewa',$data->id) }}"> Pinjam Sekarang </a>
               </div>
             </div>
           </div>
