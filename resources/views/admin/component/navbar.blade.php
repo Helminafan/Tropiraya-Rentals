bad49b5ab8dcd6741f51c198351c3e3d7983193d
Helminafan <nafan2332@gmail.com>
2023-12-17 19:14:37 +0800

    fixed

--- resources/views/admin/component/navbar.blade.php
+++ resources/views/admin/component/navbar.blade.php
@@ -41,7 +41,7 @@
       <!-- Nav Item - User Information -->
       <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <span class="mr-2 d-none d-lg-inline text-gray-900 small">Douglas McGee</span>
+          <span class="mr-2 d-none d-lg-inline text-gray-900 small">{{Auth::user()->name}}</span>
           <img class="img-profile rounded-circle" src="{{asset('admin/img/undraw_profile.svg')}}" />
         </a>
         <!-- Dropdown - User Information -->
