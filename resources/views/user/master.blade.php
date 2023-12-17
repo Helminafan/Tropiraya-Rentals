f3df621f8b626c5be9d86042e0244e724f6f8849
Mohammadananta <anantamohammad2003@gmail.com>
2023-12-17 14:15:04 +0700

    up

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -53,7 +53,7 @@
     <!-- Navbar Start -->
     <header class="container-fluid nav-bar bg-transparent">
         <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
-            <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
+            <a href="{{ route('user.index') }}" class="navbar-brand d-flex align-items-center text-center">
                 <div class="icon p-2 me-2">
                     <img class="img-fluid" src="{{ asset('landingPage/img/icon-deal.png') }}" alt="Icon"
                         style="width: 30px; height: 30px;">
